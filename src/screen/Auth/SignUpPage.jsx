41c8d2a2b782059364db04d9edd453e83027472b
MgHtetMyatAung <htetmyataung16000@gmail.com>
2023-05-05 01:25:01 +0630

    morning-validation-form

--- src/screen/Auth/SignUpPage.jsx
+++ src/screen/Auth/SignUpPage.jsx
@@ -1,147 +1,157 @@
-import React from 'react';
+import React from "react";
 import Logo from "../../assets/logo.png";
-import { useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const SignUpSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too short!")
+    .max(50, "Too long!")
+    .required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().min(8, "Password must be at least 8 characters"),
+  password_confirmation: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Password confirmation is required"),
+});
 
 const SignUpPage = () => {
-    const formik= useFormik({
-        initialValues:{
-            name:'',
-            email:'',
-            password:'',
-            password_confirmation:''
-        },
-        onSubmit:(values)=>{
-            console.log(values);
-            formik.resetForm();
-        }
-    })
-    const navigate= useNavigate();
-    return (
-        <>
-          <div className="flex min-h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <img
-                className="mx-auto h-20 w-auto"
-                src={Logo}
-                alt="Your Company"
-              />
-              <h2 className="mt-5 text-center text-2xl font-semibold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
-              </h2>
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    },
+    validationSchema: SignUpSchema,
+    onSubmit: (values,{setSubmitting}) => {
+      console.log(values);
+      formik.resetForm();
+    },
+  });
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex min-h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-20 w-auto" src={Logo} alt="Your Company" />
+          <h2 className="mt-5 text-center text-2xl font-semibold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-2" onSubmit={formik.handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Your Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                />
+              </div>
             </div>
-    
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-2" onSubmit={formik.handleSubmit}>
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Your Name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="name"
-                      name="name"
-                      type="text"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
-                      onChange={formik.handleChange}
-                      value={formik.values.name}
-                    />
-                  </div>
-                </div>
 
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
-                      onChange={formik.handleChange}
-                      value={formik.values.email}
-                    />
-                  </div>
-                </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                />
+              </div>
+            </div>
 
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="password"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
-                      onChange={formik.handleChange}
-                      value={formik.values.password}
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Confirm Password
-                    </label>
-                  </div>
-                  <div className="mt-2">
-                    <input
-                      id="password_confirmation"
-                      name="password_confirmation"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
-                      onChange={formik.handleChange}
-                      value={formik.values.password_confirmation}
-                    />
-                  </div>
-                </div>
-    
-                <div className="pt-5">
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Sign in
-                  </button>
-                </div>
-              </form>
-    
-              <p className="mt-10 text-center text-sm text-gray-500">
-                You have already an account?{" "}
-                <a
-                  href="#"
-                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ms-5"
-                  onClick={()=>navigate('/login')}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Sign In
-                </a>
-              </p>
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password_confirmation"
+                  name="password_confirmation"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  onChange={formik.handleChange}
+                  value={formik.values.password_confirmation}
+                />
+              </div>
             </div>
-          </div>
-        </>
-      );
-}
 
-export default SignUpPage
+            <div className="pt-5">
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            You have already an account?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ms-5"
+              onClick={() => navigate("/login")}
+            >
+              Sign In
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUpPage;
